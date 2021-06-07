38bedcbfa8b88e9d1632e2bf23a26c892e0e2bfd
Ben Hopkins <bjhops17@gmail.com>
2021-06-07 16:12:21 -0700

    added positioning for login button

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,48 +1,45 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import "./style.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 export default function index() {
-    return (
-        <div className="headerPhoto">
-
+  return (
+    <div className="headerPhoto">
+      
         <div id="CardContain">
-        <div id="photoCard">
-        <Link  className="ImgLink" to="/photos">
-        <br></br>
-            <div className="imgContain" >
-                <div id="photoImg" alt="Photo Link" >
-                <h1 className="cardText" >Photos</h1>
+          <div id="photoCard">
+            <Link className="ImgLink" to="/photos">
+              <br></br>
+              <div className="imgContain">
+                <div id="photoImg" alt="Photo Link">
+                  <h1 className="cardText">Photos</h1>
                 </div>
-           
-              
-            </div>
-            <br></br>
+              </div>
+              <br></br>
             </Link>
-        </div>
-       
-            <div type="button" id="videoCard">
+          </div>
+
+          <div type="button" id="videoCard">
             <Link className="ImgLink" to="/videos">
-            <br></br>
-            <div className="imgContain">
-                 <div id="videoImg" alt="Video Link">
-                     <h1 className="cardText" >Video</h1>
-                 </div>
-                
+              <br></br>
+              <div className="imgContain">
+                <div id="videoImg" alt="Video Link">
+                  <h1 className="cardText">Video</h1>
+                </div>
+              </div>
 
-            </div>
-
-            <br></br>
+              <br></br>
             </Link>
-            </div>
-            
+          </div>
+          <div id="ownerLinkContain">
+          <Link id="ownerAccess" to="/AdminLogin">
+            <p>Owner</p>
+          </Link>
+          </div>
 
+         
         </div>
-        
-
-
-
-        </div>
-            
        
-    )
+      
+    </div>
+  );
 }
